f779950ca591a51949181bc6d2d03f7f0076d794
Kayk-Rios <manjac688@gmail.com>
2024-12-31 20:44:53 -0300

    chore:  inicializandoProjeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, ViewEncapsulation  } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MenuComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.None, 
+})
+export class AppComponent {
+  title = 'Gal Store';
+  isMenuActive = false;
+
+  toggleMenu(): void {
+    this.isMenuActive = !this.isMenuActive;
+  }
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+      // {
+    //     path:'',
+    //     component: HomeComponent
+    // }
+    // ,{
+    //     path:'contato',
+    //     component: ContatoComponent
+
+    // },
+    // {
+    //     path:'sobre',
+    //     component: SobreComponent
+    // },
+    // {
+    //     path:'blusas',
+    //     component: BlusasComponent
+    // },
+    // {
+    //     path:'calcas',
+    //     component: CalcasComponent
+    // }
+];
