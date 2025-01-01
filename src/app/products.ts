ccd59477bbd4a237610f2ef1c9af1a02527b44d9
Kayk-Rios <manjac688@gmail.com>
2025-01-01 03:00:08 -0300

    Feat:produtosLista

---
+++ src/app/products.ts
@@ -0,0 +1,86 @@
+export const PRODUCTS = [
+  {
+    id: 1,
+    title: 'Calça jeans',
+    description: 'Calça jeans de corte reto, ideal para o dia a dia. Confortável, resistente e perfeita para diferentes estilos.',
+    category: 'Calça',
+    images: ['assets/Calçajens-1.png']
+  },
+  {
+    id: 2,
+    title: 'Calça saruel',
+    description: 'Calça saruel em tecido leve e confortável, perfeita para quem busca um estilo mais descontraído e ao mesmo tempo elegante.',
+    category: 'Calça',
+    images: ['assets/CalçaSaruel.png']
+  },
+
+
+
+
+  {
+    id: 3,
+    title: 'Sandalha havaiana',
+    description: 'Sandalha havaiana clássica, ideal para os dias quentes de verão. Confortável e prática, perfeita para a praia ou passeio casual.',
+    category: 'Sandalha',
+    images: ['assets/Sandalhahavaiana.png']
+  },
+  {
+    id: 4,
+    title: 'Sandalha de tiras',
+    description: 'Sandalha de tiras finas, ideal para ocasiões mais formais ou para quem busca um estilo sofisticado e confortável.',
+    category: 'Sandalha',
+    images: ['assets/Sandalhadetiras.png']
+  },
+
+
+  {
+    id: 5,
+    title: 'Camisa polo',
+    description: 'Camisa polo de algodão, perfeita para um look casual e elegante. Conforto garantido para o dia a dia.',
+    category: 'Camisa',
+    images: ['assets/Camisapolo.png']
+  },
+  {
+    id: 6,
+    title: 'Camisa de manga curta',
+    description: 'Camisa de manga curta, leve e fresca, ideal para o verão. Com design moderno, é uma peça essencial para o seu guarda-roupa.',
+    category: 'Camisa',
+    images: ['assets/Camisademangacurta.png']
+  },
+
+
+
+  {
+    id: 7,
+    title: 'Vestido de praia',
+    description: 'Vestido leve e arejado, perfeito para os dias de verão. Ideal para passeios à beira-mar ou para relaxar em um dia quente.',
+    category: 'Vestido',
+    images: ['assets/Vestidodepraia.png']
+  },
+  {
+    id: 8,
+    title: 'Vestido longo',
+    description: 'Vestido longo e fluido, perfeito para eventos noturnos ou ocasiões especiais. Elegante e confortável.',
+    category: 'Vestido',
+    images: ['assets/Vestidolongo.png']
+  },
+
+
+
+  {
+    id: 9,
+    title: 'Cinto de couro',
+    description: 'Cinto de couro premium, ideal para completar o look com estilo. Durável e sofisticado, combina com diversos tipos de roupas.',
+    category: 'Sinto',
+    images: ['assets/Cintodecouro.png']
+  },
+  {
+    id: 10,
+    title: 'Cinto de camurça',
+    description: 'Cinto de camurça macia, perfeito para adicionar um toque de elegância ao visual. Leve e durável, ideal para diferentes ocasiões.',
+    category: 'Sinto',
+    images: ['assets/Cintodecamurça.png']
+  }
+
+  
+];
