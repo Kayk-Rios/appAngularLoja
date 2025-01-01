09cdc177bf190d71fcd0ee04e20dda7309a8aa1a
Kayk-Rios <manjac688@gmail.com>
2025-01-01 02:59:50 -0300

    Feat: addCardProdutos

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { PRODUCTS } from '../../products';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+
+  products = PRODUCTS;
+  filteredProducts = PRODUCTS;
+  categories = [...new Set(PRODUCTS.map(product => product.category))];
+  accordionStates: boolean[] = Array(this.products.length).fill(false);
+
+  filterByCategory(category: string): void {
+    if (category === 'Todos') {
+      this.filteredProducts = this.products;
+    } else {
+      this.filteredProducts = this.products.filter(
+        product => product.category === category
+      );
+    }
+    this.accordionStates = Array(this.filteredProducts.length).fill(false);
+  }
+  toggleAccordion(index: number): void {
+    this.accordionStates[index] = !this.accordionStates[index];
+  }
 
 }
