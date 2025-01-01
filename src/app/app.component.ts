0707fc7fcc5ec89bc5d23f8d068eaf06291429de
Kayk-Rios <manjac688@gmail.com>
2025-01-01 03:01:24 -0300

    Feat: usandoCardProduto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, ViewEncapsulation  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent, FooterComponent],
+  imports: [RouterOutlet, MenuComponent, FooterComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   encapsulation: ViewEncapsulation.None, 
